6e6eb703bc2c5e9db9d5f061bf42c413c0213227
ITadvocate <69198345+ITadvocate@users.noreply.github.com>
2023-09-20 15:03:11 +0530

    Update atmdirect.groovy

--- atmdirect.groovy
+++ atmdirect.groovy
@@ -12,7 +12,7 @@ pipeline {
 
 //   }
 
-  agent any 
+  agent { any }
 
   stages {
 
@@ -33,11 +33,37 @@ pipeline {
 
     stage ("Updating github for ArgoCD") {
      steps {
-        dir ("atm"){
-        sh "helm upgrade --install atm_demo . values.yaml" + " --set application.docker.tag=${currentBuild.number}"
+        dir ('atm') {
+        sh"cat demo_values > values.yaml"
+        sh """sed -i "s/v1/v${BUILD_NUMBER}/" values.yaml"""
+        }
      }
-    }
   }
+
+
+
+    stage ('Git-Commit push ****************') {
+                    steps {
+                            dir ('atm') {
+                                script {
+                                    withCredentials([gitUsernamePassword(credentialsId: 'GITHUB_TOKEN', gitToolName: 'Default')]) {
+                                        sh "git config --global user.email 'vikram@test.com'"
+                                        sh "git config --global user.name 'VicK'"
+                                        sh "git add values.yaml"
+                                        sh "git config --global --add safe.directory '*'"
+                                        sh "git pull --set-upstream origin master"
+                                        sh "git commit -m 'pushing test build: ${BUILD_NUMBER}"
+                                        sh "git pull --set-upstream origin master"
+                                        sh "git branch"
+                                        sh "git push --set-upstream origin -f ${groovy_branch}"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                
+
+
 
 }
 }
